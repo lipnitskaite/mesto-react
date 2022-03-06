fa0be6080d85d2eb71c59ea13c34fd0bea26510d
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-03-06 22:37:47 +0300

    feat: handle close popup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,23 @@
+import React from 'react';
+
 function Main({
-  handleEditAvatarClick
+  onEditAvatar,
+  onEditProfile,
+  onAddPlace,
 }) {
     return (
       <main className="content">
         <section className="profile">
           <div className="profile__photo-container">
             <img src='#' className="profile__photo" alt=""/>
-            <button className="profile__photo-edit-button" type="button" onClick={handleEditAvatarClick}></button>
+            <button className="profile__photo-edit-button" type="button" onClick={onEditAvatar}></button>
           </div>
           <div className="profile__main">
             <h1 className="profile__title">Жак-Ив Кусто</h1>
-            <button className="profile__edit-button" type="button"></button>
+            <button className="profile__edit-button" type="button" onClick={onEditProfile}></button>
           </div>
           <p className="profile__subtitle">Исследователь океана</p>
-          <button className="profile__add-button" type="button"></button>
+          <button className="profile__add-button" type="button" onClick={onAddPlace}></button>
         </section>
 
         <section className="places page__places"></section>
